179e1c3fd1271b076284169c44ff12e312ee259d
wallirashid <wallirashid10@gmail.com>
2020-01-20 13:13:34 +0500

    Admin Panel is ready

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="users-table">
+        <table class="table table-responsive table-bordered">
+            <thead>
+                <tr>
+                    <td>Id</td> 
+                    <td>username</td> 
+                    <td>email</td> 
+                    <td>Created At</td>
+                    <td>Updated At</td>    
+                </tr>    
+            </thead>            
+            <tbody>
+                @if($users)
+                    @foreach($users as $user )
+                        <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->created_at}}</td>
+                            <td>{{$user->updated_at}}</td>
+
+                        </tr>
+                    @endforeach
+                @endif    
+
+
+            </tbody>
+
+        </table>
+
+    </div>
+
+@endsection
